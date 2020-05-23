e0058d790e9752ca65a6ef6ee07cc0360a74074c
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-05-23 16:56:14 +0530

    removed unwanted images, updated logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,38 +19,38 @@ const Layout = ({ children }) => {
                 <html lang="en" />
                 <title>{title}</title>
                 <meta name="description" content={description} />
-
+                {/* <link rel="manifest" href="/site.webmanifest" /> */}
                 <link
                     rel="apple-touch-icon"
-                    sizes="180x180"
-                    href={`${withPrefix("/")}img/apple-touch-icon.png`}
+                    // sizes="180x180"
+                    href={`${withPrefix("/")}img/om-logo.png`}
                 />
                 <link
                     rel="icon"
                     type="image/png"
-                    href={`${withPrefix("/")}img/favicon-32x32.png`}
-                    sizes="32x32"
+                    href={`${withPrefix("/")}img/om-logo.png`}
+                    // sizes="32x32"
                 />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    href={`${withPrefix("/")}img/favicon-16x16.png`}
-                    sizes="16x16"
-                />
-
+                {/* <link */}
+                {/*     rel="icon" */}
+                {/*     type="image/png" */}
+                {/*     href={`${withPrefix("/")}img/favicon-16x16.png`} */}
+                {/*     sizes="16x16" */}
+                {/* /> */}
                 <link
                     rel="mask-icon"
-                    href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
-                    color="#ff4400"
+                    href={`${withPrefix("/")}img/om-logo.png`}
+                    // color="#ff4400"
                 />
                 <meta name="theme-color" content="#fff" />
-
-                <meta property="og:type" content="business.business" />
+                <meta name="msapplication-TileColor" content="#fff" />
+                <meta property="og:type" content="website" />
                 <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
                 <meta property="og:url" content="/" />
                 <meta
                     property="og:image"
-                    content={`${withPrefix("/")}img/og-image.jpg`}
+                    content={`${withPrefix("/")}img/om-logo.png`}
                 />
             </Helmet>
             <Navbar title={title} />
