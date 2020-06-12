53c333030b2aa6209ba43294c7ba71e53f12847f
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-06-12 21:08:55 +0530

    added all colors to theme

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -2,12 +2,24 @@ const darkTheme = {
     '--dark-color': 'black',
     '--light-color': 'white',
     '--light-color-3': 'rgba(255,255,255,0.5)',
+    '--shadow-color': 'rgba(128,128,128)',
+    '--shadow-color-3': 'rgba(128,128,128,0.3)',
 };
+
+// const darkTheme = {
+//     '--dark-color': '#0F3B5C',
+//     '--light-color': '#58B2F5',
+//     '--light-color-3': 'rgba(88, 178, 245, 0.5)',
+//     '--shadow-color': '#2C73A8',
+//     '--shadow-color-3': 'rgba(44, 115, 168, 0.3)',
+// };
 
 const lightTheme = {
     '--dark-color': 'white',
     '--light-color': 'black',
     '--light-color-3': 'rgba(0,0,0,0.5)',
+    '--shadow-color': 'rgba(128,128,128)',
+    '--shadow-color-3': 'rgba(128,128,128,0.3)',
 };
 
 export const applyTheme = (nextTheme) => {
